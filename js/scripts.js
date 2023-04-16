cee25ec3087f1738ae2168da8b3aefa826d1731d
meloflaherty <melindaoflaherty@gmail.com>
2023-04-16 14:38:19 -0700

    Hamburger navigation across all pages
    Created functional navigation with javascript toggle on/off code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log("Hi there! Welcome to my online portfolio!")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+}
+
+/* to hide or show the menu on click */
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
